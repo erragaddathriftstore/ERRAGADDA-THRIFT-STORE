df545856bb17e2e7fa91c7934d98d4611428f637
erragaddathriftstore <erragaddathriftstore@gmail.com>
2026-03-17 01:24:37 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
 fetch("data/products.json")
-  .then(response => response.json())
-  .then(data => {
+.then(response => response.json())
+.then(data => {
 
-    const container = document.getElementById("product-list");
+  const container = document.getElementById("product-list");
 
-    data.products.forEach(product => {
+  data.products.forEach(product => {
 
-      const card = document.createElement("div");
-      card.classList.add("product-card");
+    const card = document.createElement("div");
 
-      card.innerHTML = `
-        <img src="${product.images[0]}" alt="${product.name}" width="200">
-        <h3>${product.name}</h3>
-        <p>₹${product.price}</p>
-        <a href="product.html?id=${product.id}">View Product</a>
-      `;
+    card.innerHTML = `
+      <img src="${product.images[0]}" width="200">
+      <h3>${product.name}</h3>
+      <p>₹${product.price}</p>
+      <a href="product.html?id=${product.id}">View Product</a>
+    `;
 
-      container.appendChild(card);
-
-    });
+    container.appendChild(card);
 
   });
+
+});
